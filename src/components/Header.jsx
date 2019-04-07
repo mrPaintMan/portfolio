21622fdf1450a1ffb82d246314ab6d2044a0465f
Filip Palmqvist <fille.palmqvist@icloud.com>
2019-04-07 18:38:33 +0200

    Initial code commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./css/Header.css";
+import BackgroundPic from "../assets/background.png"
+import mePic from "../assets/me.png"
+
+class Header extends React.Component {
+    
+    render() {
+        return (
+            <div className="header">
+                
+                <div className="menu">
+                    <div className="menu_item">Filip</div>
+                    <img src={mePic} className="mePic" alt=""/>
+                    <div className="menu_item">Palmqvist</div>
+                </div>
+
+                <img src={BackgroundPic} className="backPic" alt=""/>
+            </div>
+        );
+    }
+}
+
+export default Header;
